dcfa16ad7b763f1e207a7de4cfb9dd2458c8c43f
Bruno Neckel <neckel.bw@gmail.com>
2025-05-07 20:59:25 -0300

    feat: add direnv configuration and environment variables management

---
+++ internal/env/env.go
@@ -0,0 +1,43 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetString(key string, fallback string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	return val
+}
+
+func GetInt(key string, fallback int) int {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	num, err := strconv.Atoi(val)
+	if err != nil {
+		return fallback
+	}
+
+	return num
+}
+
+func GetBool(key string, fallback bool) bool {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+
+	parsed, err := strconv.ParseBool(val)
+	if err != nil {
+		return fallback
+	}
+
+	return parsed
+}
